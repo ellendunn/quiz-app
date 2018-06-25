6e2603e6776afdc74e905f338529305a0dedd480
Ellen Dunn <edunn227@gmail.com>
2018-06-25 16:56:11 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,153 @@
+'use strict';
+
+let questionNumber = 0;
+let score = 0;
+// const question = QUIZ[questionNumber];
+
+
+//start quiz!
+function startQuiz() {
+  //user clicks start button to go to the first question.
+  $('.container').on('click', '.js-start-button', event => {
+    $('.container').hide();
+     renderQuestion()
+  }); 
+} 
+
+function renderQuestion(){
+$('.questionContainer').show();
+$('.questionContainer').html(questionTemplate());
+updateQuestionTracking(questionNumber+1);
+}
+
+function questionTemplate(){
+  // score, question, questionTracking
+  //this function will load the question page with the question, all options, and a select button
+if (questionNumber === 10){
+  renderFinalResults()
+  }
+else {
+  $('.questionContainer').addClass('activeQuestion');
+  return(`<div class = 'questionTitle'>
+     <h1>${QUIZ[questionNumber].text}</h1>
+  </div>
+  <form class='questionForm'>
+    <fieldset name='answer-options'>
+    <legend></legend>
+    <input type='radio' name='option' id='option-1' value='${QUIZ[questionNumber].ans1}' required>
+    <label for='option-1'>${QUIZ[questionNumber].ans1}</label><br>  
+    <input type='radio' name='option' id='option-2' value='${QUIZ[questionNumber].ans2}' required>
+    <label for='option-2'>${QUIZ[questionNumber].ans2}</label><br>
+    <input type='radio' name='option' id='option-3' value='${QUIZ[questionNumber].ans3}' required>
+    <label for='option-3'>${QUIZ[questionNumber].ans3}</label><br>
+    <input type='radio' name='option' id='option-4' value='${QUIZ[questionNumber].ans4}' required>
+    <label for='option-4'>${QUIZ[questionNumber].ans4}</label><br> 
+  </fieldset>
+  <button type='submit' class = 'submitAnswer' >Submit</button>
+  </form>`)
+}
+}
+
+
+function handleNextButton(){
+  //listens for event on results page that will load the next question and go back to renderQuestion function. 
+  $('.questionContainer').on('click', '.js-next-button', event => {
+
+    questionNumber++;
+    renderQuestion();
+  }
+  )}
+
+
+function updateScore(){
+  score ++;
+  $('.score').closest('span').text(score);
+}
+
+function updateQuestionTracking(q){
+  $('.questionTracking').closest('span').text(q);
+}
+
+function handleSubmitButton(){
+  //listen for an event after selecting an option and then clicking submit button that will lead to results of the question
+  $('.questionContainer').on('click', '.submitAnswer', event => {
+    event.preventDefault();
+      renderResults();
+  }); 
+}
+  
+
+function renderResults(){
+  //load results to tell user if they selected the correct answer or the wrong answer. This will also update the quiz tracking and score.
+    
+    let selected = $('input:checked');
+    let selectedAnswer = selected.val();
+    let correctAnswer=`${QUIZ[questionNumber].correctAnswer}`;
+
+    if (selectedAnswer === correctAnswer){
+      $('form').hide();
+      $('.questionTitle').hide()
+      $('.questionContainer').append(
+        `<h1>You got it right!</h1>
+		      <img class='resultsGif' src="https://78.media.tumblr.com/b641e5f3475c9cbd871d4f4f4498cac7/tumblr_npahsmfo4l1sxn2y0o7_500.gif" alt="Wayne Brady saying 'Yaaaay!' and dancing"><br>
+    	    <button type="button" class="js-next-button">Yes, and...</button>`);
+        updateScore();
+      }
+    else{
+      $('form').hide();
+      $('.questionTitle').hide()
+      $('.questionContainer').append(
+        `<h1>Nope! I think you meant to pick '${QUIZ[questionNumber].correctAnswer}'</h1>
+	      	<img class='resultsGif' src="http://i0.kym-cdn.com/photos/images/original/000/510/114/f14.gif" alt="Colin Mochrie making a silly face"><br>
+        	<button type="button" class="js-next-button">Yes, and...</button>`
+      )}
+  }
+
+function renderFinalResults(){
+  //after all questions have been answered, this will accumulate results and display how the user should proceed in their improv career.
+
+$('.questionContainer').addClass('finalResultsContainer');
+
+if (score >= 8){
+  $('.heading').hide();
+  $('.questionContainer').html(`
+  <h1>You got ${score} questions correct!</h1><h2>Try your shot on stage! Lorne Michaels might be watching!</h2>
+    <img class = 'resultsGif' src = "https://78.media.tumblr.com/d104454b2ec3d885f369a90ca80d6e55/tumblr_n7agy0O7Fk1tplkyfo1_400.gif" alt="Wayne Brady gif saying 'When it comes to improv on the tops we sits'"><br>
+    <button type="button" class="js-restart-button">Restart Quiz!</button>`)
+}
+else if (score <8 && score >=5){
+    $('.heading').hide();
+    $('.questionContainer').html(`<h1>You got ${score} questions correct!</h1><h2>Have you been taking classes? You may have a nack for this stuff!</h2>
+		<img class = 'resultsGif' src="https://i.gifer.com/W3If.gif" alt="Wayne Brady gif saying 'you all get it'"><br>
+    <button type="button" class="js-restart-button">Restart Quiz!</button>`)
+}
+else {
+    $('.heading').hide();
+    $('.questionContainer').html(`<h1>You got ${score} questions correct!</h1><h2>You may want to start with the basics. Try some improv classes at your nearest theater!</h2>
+  		<img class = 'resultsGif' src='http://31.media.tumblr.com/d5a54fcf6460728f38b397226b628289/tumblr_mmd9ltjQs11r6xbv8o1_250.gif' alt="Colin Mochrie gif saying 'That was horrible!'"><br>
+    <button type="button" class="js-restart-button">Restart Quiz!</button>`)
+}
+}
+
+function handleRestartButton(){
+  $('.questionContainer').on('click', '.js-restart-button', event => {
+    // location.reload();
+    questionNumber = 0;
+    score = 0;
+    $('.container').show();
+    $('.questionContainer').hide();
+    // startQUIZ();
+  });
+}
+
+
+function runQuiz(){
+  startQuiz();
+  handleSubmitButton ();
+  handleNextButton();
+  handleRestartButton();
+}
+
+$(runQuiz())
+
+
---
+++ questionset.js
@@ -0,0 +1,96 @@
+const QUIZ = [
+{
+number: 1,
+question: 'What is necessary for a scene to be termed "Improvisation"?',
+ans1: 'It has to be funny.',
+ans2: 'It needs at least two people.',
+ans3: 'Everything must be made up on the spot.',
+ans4: 'People need to be watching it.',
+correctAnswer: 'Everything must be made up on the spot.'
+},
+{
+number: 2,
+question: 'What is a "Back Line" in Improv?',
+ans1: 'A game where all of the imrprovsers  on stage do a Conga Line backwards',
+ans2: 'A term for when someone says a line with their back to the audience',
+ans3: 'A game where everyone must say their sentences backwards',
+ans3: 'When all improvisers not in a scene line up along the back of the stage',
+correctAnswer: 'When all improvisers not in a scene line up along the back of the stage'
+},
+{
+number: 3,
+question: 'Which of these famous comedians did NOT graduate from the Second City Training Program?',
+ans1: 'Steve Carell',
+ans2: 'Tina Fey',
+ans3: 'Bill Murray',
+ans4: 'Amy Schumer',
+correctAnswer: 'Amy Schumer'
+},
+{
+number: 4,
+question: 'What three things are essential to the start of a good improv scene?',
+ans1: 'Who, What, Where',
+ans2: 'Characters, Accents, Object work',
+ans3: 'One Improviser, A Suggestion From the Audience, A Stage',
+ans4: 'Two Improvisers, A Chair on Stage, A Back Line',
+correctAnswer: 'Who, What, Where'
+},
+{
+number: 5,
+question: 'Which of the following terms is a method for editing a scene?',
+ans1: '"Tossing the Scene"',
+ans2: '"Sweeping"',
+ans3: '"Making New"',
+ans4: '"Creationism"',
+correctAnswer: '"Sweeping"'
+},
+{
+number: 6,
+question:'Which of the following terms is NOT an editing method?',
+ans1: 'Tag Out',
+ans2: 'Focus Edit',
+ans3: 'Twist In',
+ans4: 'Revolving Door',
+correctAnswer: 'Twist In'
+},
+{
+number: 7,
+question:'What is the main strategy for being a good improviser?',
+ans1: 'Saying "Yes, and..."',
+ans2: 'Being funny',
+ans3: 'Looking good',
+ans4: 'Having a few drinks before performing',
+correctAnswer: 'Saying "Yes, and..."'
+},
+{
+number: 8,
+question:'What are the basic rules for the classic game of "Freeze"?',
+ans1: 'Everyone on stage must perform a scene without moving.',
+ans2: 'New improvisers must take the physical position of the people in the current scene and start a new scene.',
+ans3: 'Improvisers must do a scene where they only talk about frozen foods.',
+ans4: 'The scene must take place in a really cold setting.',
+correctAnswer: 'New improvisers must take the physical position of the people in the current scene and start a new scene.'
+},
+{
+number: 9,
+question:'If a scene is not going anywhere, what can you do to save it?',
+ans1: 'Panic and run off stage',
+ans2: 'Kill off your character',
+ans3: 'Heighten your emotional response',
+ans4: 'Nothing, it cannot be saved',
+correctAnswer: 'Heighten your emotional response'
+},
+{
+number: 10,
+question:'On stage, what is the cue that a scene or game is over?',
+ans1: 'The lights go off or the host calls "Scene!"',
+ans2:'All performers look to the audience and say "We\'re done!"',
+ans3: 'The performers walk off stage.',
+ans4: 'A character dies.',
+correctAnswer: 'The lights go off or the host calls "Scene!"'
+}
+]
+
+
+
+
